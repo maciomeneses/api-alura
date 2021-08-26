4bcb18bd072ee9339e2cc906f0b3a8acdfc5c0e9
Maxel-Uds <maxellopes32@gmail.com>
2021-08-26 14:19:47 -0300

    Projeto criado Gradle Spring Initializer

---
+++ alura/src/main/java/src/main/java/br/com/alura/curso/AluraApplication.java
@@ -0,0 +1,13 @@
+package src.main.java.br.com.alura.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluraApplication.class, args);
+	}
+
+}
