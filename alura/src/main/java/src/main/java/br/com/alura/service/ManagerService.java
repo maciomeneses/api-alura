c54b1e013d59f684f468fcaeece54d79b3bc397d
Maxel-Uds <maxellopes32@gmail.com>
2021-09-01 15:27:59 -0300

    Construída a lógica de adicionar um curso à lista de cursos da classe CourseStorageService

--- alura/src/main/java/src/main/java/br/com/alura/service/ManagerService.java
+++ alura/src/main/java/src/main/java/br/com/alura/service/ManagerService.java
@@ -1,10 +1,13 @@
 package src.main.java.br.com.alura.service;
 
 import src.main.java.br.com.alura.dto.CourseApprovalDto;
+import src.main.java.br.com.alura.dto.CourseDetails;
 import src.main.java.br.com.alura.dto.ManagerDTO;
 import src.main.java.br.com.alura.modelo.Course;
 
 public class ManagerService {
+
+    private Course course;
 
     public void ApproveCourse(CourseApprovalDto courseApproval, ManagerDTO manager) {
 
@@ -12,19 +15,13 @@ public class ManagerService {
             //devolver erro
         } ;
             //atualizar valor no banco de dados no course aproval
-            //Adiciona o curso aprovado na lista de cursos
-
-
-
-
-
+            //Adiciona o curso aprovado na lista de cursos:
+            CourseStorageService.addCourse(new CourseDetails(course));
     }
 
-    public void CreateCourse( Long id,String name, String category,int workload){
+    public void CreateCourse( Long id,String name, String category,int workload) {
 
-
-
-        Course curso = Course.builder().id(id).name(name).category(category).workload(workload).build();
+         course = Course.builder().id(id).name(name).category(category).workload(workload).build();
         //passar o curso para o banco de dados
     }
 
